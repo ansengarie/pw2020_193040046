e9fa5ab08d3edcd323c833f05d57249b65995d49
ajins15 <ansengarie@gmail.com>
2020-04-20 23:04:26 +0700

    latihan 1

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,54 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '', 'pw_193040046');
+
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+
+$mahasiswa = $rows;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiswa as $mhs) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $mhs['gambar']; ?>"></td>
+        <td><?= $mhs['nrp']; ?></td>
+        <td><?= $mhs['nama']; ?></td>
+        <td><?= $mhs['email']; ?></td>
+        <td><?= $mhs['jurusan']; ?></td>
+        <td>
+          <a href="">ubah</a> | <a href="">hapus</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
