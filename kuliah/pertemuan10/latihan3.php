41c17f6ec7f13243e08420c1758d5eea4f0fd649
ajins15 <ansengarie@gmail.com>
2020-04-22 22:54:52 +0700

    UI latihan 3

--- kuliah/pertemuan10/latihan3.php
+++ kuliah/pertemuan10/latihan3.php
@@ -10,30 +10,63 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Daftar Mahasiswa</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-  <h3>Daftar Mahasiswa</h3>
+  <!-- Navbar -->
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      Daftar Mahasiswa
+    </a>
+    <p>
+      <a href="tambah.php" class="btn btn-success mt-2">Tambah Data Mahasiswa</a>
+    </p>
+  </nav>
 
-  <table border="1" cellpadding="10" cellspacing="0">
-    <tr>
-      <th>#</th>
-      <th>Gambar</th>
-      <th>Nama</th>
-      <th>Aksi</th>
-    </tr>
-    <?php $i = 1;
-    foreach ($mahasiswa as $mhs) : ?>
-      <tr>
-        <td><?= $i++; ?></td>
-        <td><img src="img/<?= $mhs['gambar']; ?>" width="60px"></td>
-        <td><?= $mhs['nama']; ?></td>
-        <td>
-          <a href="detail.php?id=<?= $mhs['id']; ?>">lihat detail</a>
-        </td>
-      </tr>
-    <?php endforeach; ?>
-  </table>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <?php foreach ($mahasiswa as $mhs) : ?>
+        <div class="col-md-4">
+          <p class="nama">
+            <div class="card mt-3  mb-4 ml-5" style="width: 18rem;">
+              <img class="card" src="img/<?= $mhs['gambar']; ?>" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title"><?= $mhs['nama']; ?></h5>
+                <a href="detail.php?id=<?= $mhs['id']; ?>" class="btn btn-primary">Lihat detail</a>
+              </div>
+            </div>
+
+          </p>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+
+  <!-- footer -->
+  <footer class="text-white bg-secondary mt-5">
+    <div class="container">
+      <div class="row pt-4">
+        <div class="col text-left">
+          <p>Copyright 2019.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <script>
+    < script src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity = "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin = "anonymous" >
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </script>
+
 </body>
 
 </html>
