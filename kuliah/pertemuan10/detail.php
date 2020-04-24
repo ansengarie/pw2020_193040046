e52bb5e87ef6b1e968e8c64575467619260c1cd0
ajins15 <ansengarie@gmail.com>
2020-04-24 07:46:12 +0700

    latihan 6a

--- kuliah/pertemuan10/detail.php
+++ kuliah/pertemuan10/detail.php
@@ -54,6 +54,7 @@ $mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
       </div>
     </div>
   </footer>
+
   <script>
     < script src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"
     integrity = "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
