90889c4d5cb445b7237e1587b1419661e905f4b7
ajins15 <ansengarie@gmail.com>
2020-05-15 23:10:20 +0700

    membuat halaman index

--- tubes/index.php
+++ tubes/index.php
@@ -1,18 +1,203 @@
 <?php
+require 'php/functions.php';
+
+$elektronik = query("SELECT * FROM elektronik");
+
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <title>Index</title>
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="fonts/font-awesome.min.css">
+  <link rel="stylesheet" href="fonts/ionicons.min.css">
+  <link rel="stylesheet" href="css/Brands.css">
+  <link rel="stylesheet" href="css/Footer-Dark.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+  <link rel="stylesheet" href="css/Lightbox-Gallery.css">
+  <link rel="stylesheet" href="css/Navigation-with-Search.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <style>
+    html {
+      scroll-behavior: smooth;
+    }
+  </style>
 </head>
 
 <body>
-
+  <nav class="navbar navbar-light navbar-expand-md navigation-clean-search" id="page-top">
+    <div class="container"><a class="navbar-brand" href="#"><img src="assets/img/logo.png" width="100px" alt=""></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav">
+          <li class="nav-item" role="presentation"><a class="nav-link" href="#Produk">Produk</a></li>
+          <li class="nav-item" role="presentation"><a class="nav-link" href="#">About</a></li>
+          <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact Us</a></li>
+        </ul>
+        <form class="form-inline mr-auto" target="_self">
+          <div class="form-group">
+            <label for="search-field">
+              <i class="fa fa-search"></i>
+            </label>
+            <input class="form-control search-field" type="search" id="search-field" name="search">
+          </div>
+        </form>
+        <a class="btn btn-light action-button" role="button" data-bs-hover-animate="pulse" href="php/login.php" style="background-color: rgb(255,0,0);">Login</a>
+      </div>
+    </div>
+  </nav>
+  <div class="photo-gallery">
+    <div class="container">
+      <div class="intro">
+        <h2 class="text-center mb-5">Selamat Datang</h2>
+        <img src="assets/img/croods.png" class="img-fluid mt-5 mb-5" alt="">
+      </div>
+      <div class="intro">
+        <h2 class="text-center mt-5" id="Produk">Produk</h2>
+      </div>
+      <div>
+        <ul class="nav nav-tabs">
+          <li class="nav-item"><a class="nav-link active" role="tab" data-toggle="tab" href="#tab-1">Handphone</a></li>
+          <li class="nav-item"><a class="nav-link" role="tab" data-toggle="tab" href="#tab-2">Laptop</a></li>
+        </ul>
+        <div class="tab-content">
+          <div class="tab-pane active" role="tabpanel" id="tab-1">
+            <div class="card-group">
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach ($elektronik as $row) : ?>
+                    <div class="col-md-4">
+                      <div class="card mt-3">
+                        <img class="card-img-top img-fluid max-foto" src="assets/img/<?= $row['foto']; ?>" style="max-height: 200px; min-height: 200px;">
+                        <div class="card-body">
+                          <h4 class="card-title"><?= $row['seri']; ?></h4>
+                          <a href="php/detail.php?id=<?= $row['id']; ?>" class="btn btn-primary">Detail</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane" role="tabpanel" id="tab-2">
+            <div class="card-group">
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach ($elektronik as $row) : ?>
+                    <div class="col-md-4">
+                      <div class="card mt-3">
+                        <img class="card-img-top img-fluid max-foto" src="assets/img/<?= $row['foto']; ?>" style="max-height: 200px; min-height: 200px;">
+                        <div class="card-body">
+                          <h4 class="card-title"><?= $row['seri']; ?></h4>
+                          <a href="php/detail.php?id=<?= $row['id']; ?>" class="btn btn-primary">Detail</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane" role="tabpanel" id="tab-3">
+            <div class="card-group">
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach ($elektronik as $row) : ?>
+                    <div class="col-md-4">
+                      <div class="card mt-3">
+                        <img class="card-img-top img-fluid max-foto" src="assets/img/<?= $row['foto']; ?>" style="max-height: 200px; min-height: 200px;">
+                        <div class="card-body">
+                          <h4 class="card-title"><?= $row['seri']; ?></h4>
+                          <a href="php/detail.php?id=<?= $row['id']; ?>" class="btn btn-primary">Detail</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane" role="tabpanel" id="tab-4">
+            <div class="card-group">
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach ($elektronik as $row) : ?>
+                    <div class="col-md-4">
+                      <div class="card mt-3">
+                        <img class="card-img-top img-fluid max-foto" src="assets/img/<?= $row['foto']; ?>" style="max-height: 200px; min-height: 200px;">
+                        <div class="card-body">
+                          <h4 class="card-title"><?= $row['seri']; ?></h4>
+                          <a href="php/detail.php?id=<?= $row['id']; ?>" class="btn btn-primary">Detail</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane" role="tabpanel" id="tab-5">
+            <div class="card-group">
+              <div class="container-fluid">
+                <div class="row">
+                  <?php foreach ($elektronik as $row) : ?>
+                    <div class="col-md-4">
+                      <div class="card mt-3">
+                        <img class="card-img-top img-fluid max-foto" src="assets/img/<?= $row['foto']; ?>" style="max-height: 200px; min-height: 200px;">
+                        <div class="card-body">
+                          <h4 class="card-title"><?= $row['seri']; ?></h4>
+                          <a href="php/detail.php?id=<?= $row['id']; ?>" class="btn btn-primary">Detail</a>
+                        </div>
+                      </div>
+                    </div>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-dark mt-3" style="background-color: rgb(150,0,0);">
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col item social">
+            <a href="#">
+              <i class="icon ion-social-facebook"></i>
+            </a>
+            <a href="#">
+              <i class="icon ion-social-twitter"></i>
+            </a>
+            <a href="#">
+              <i class="icon ion-social-snapchat"></i>
+            </a>
+            <a href="#"><i class="icon ion-social-instagram"></i>
+            </a>
+          </div>
+        </div>
+        <p class="copyright">Jee Elektronik © 2020</p>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="js/bs-animation.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <script src="js/stylish-portfolio.js"></script>
+  <script src="js/script.js"></script>
 </body>
 
 </html>
---
+++ tubes/js/script.js
@@ -0,0 +1,7 @@
+const tombolCari = document.querySelector('.tombol-cari');
+const keyword = document.querySelector('.keyword');
+const container = document.querySelector('.container');
+
+//hilangkan tombol cari
+tombolCari.style.display = 'none';
+
